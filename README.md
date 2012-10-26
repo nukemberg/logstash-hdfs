d46427d841ec4355d803f717cd0c1979e47aa1e0
Avishai Ish-Shalom <avishai@fewbytes.com>
2012-10-26 23:00:08 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,41 @@
-logstash-hdfs
+Logstash HDFS plugin
 =============
 
-Logstash HDFS plugin
+An HDFS plugin for [Logstash](http://logstash.net). This plugin is provided as an external plugin (see Usage below) and is not part of the Logstash project.
+
+Usage
+=============
+
+run logstash with the `--pluginpath` (`-p`) command line argument to let logstash know where the plugin is. Also, you need to let Java know where your Hadoop JARs are, so set the `CLASSPATH` variable correctly.
+E.G.
+
+    java -jar logstash-1.1.3-monolithic.jar agent -f conf/hdfs-output.conf -p /path/to/cloned/logstash-hdfs
+
+
+Config options are basically the same as the file output, but have a look at the `doc/` directory for specfics. 
+
+HDFS Append and rewriting files
+=============
+Please note, HDFS versions prior to 2.x do not properly support append. See [HADOOP-8230](https://issues.apache.org/jira/browse/HADOOP-8230) for reference.
+To enable append on HDFS, set _dfs.support.append_ in <tt>hdfs-site.conf</tt> (2.x) or _dfs.support.broken.append_ on 1.x, and use the *enable_append* config option:
+
+    input {
+        hdfs {
+            path => "/path/to/output_file.log"
+            enable_append => true
+        }
+    }
+
+If append is not supported and the file already exists, the plugin will cowardly refuse to reopen the file for writing unless *enable_reopen* is set to true.
+This is probably a very bad idea, you have been warned!
+
+HFDS Flush
+=============
+Flush and sync don't actually work as promised on HDFS (see [HDFS-536](https://issues.apache.org/jira/browse/HDFS-536)).
+In Hadoop 2.x, `hflush` provides flush-like functionality and the plugin will use `hflush` if it is available.
+Nevertheless, flushing code has been left in the plugin in case `flush` and `sync` will work on some HDFS implementation.
+
+License
+=============
+The plugin is released under the LGPL v3.
+
---
+++ logstash/outputs/hdfs.rb
@@ -0,0 +1,167 @@
+require "logstash/namespace"
+require "logstash/outputs/base"
+
+# HDFS output.
+#
+# Write events to files to HDFS. You can use fields from the
+# event as parts of the filename.
+class LogStash::Outputs::HDFS < LogStash::Outputs::Base
+
+  config_name "hdfs"
+  plugin_status "beta"
+
+  # The path to the file to write. Event fields can be used here, 
+  # like "/var/log/logstash/%{@source_host}/%{application}"
+  config :path, :validate => :string, :required => true
+
+  # The format to use when writing events to the file. This value
+  # supports any string and can include %{name} and other dynamic
+  # strings.
+  #
+  # If this setting is omitted, the full json representation of the
+  # event will be written as a single line.
+  config :message_format, :validate => :string
+
+  # Flush interval for flushing writes to log files. 0 will flush on every meesage
+  # Flush doesn't actually work on most Hadoop 1.x versions. if you really care about flush, use 2.x 
+  config :flush_interval, :validate => :number, :default => 60
+
+  # Enable the use of append. This only works with Hadoop 2.x dfs.support.append or 1.x with dfs.support.broken.append
+  config :enable_append, :validate => :boolean, :default => false
+
+  # Enable re-opening files. This is a really a bad idea because HDFS will truncate files. Only use if you know what you're doing
+  config :enable_reopen, :validate => :boolean, :default => false
+
+  public
+  def register
+    require "java"
+    java_import "org.apache.hadoop.fs.Path"
+    java_import "org.apache.hadoop.fs.FileSystem"
+    java_import "org.apache.hadoop.conf.Configuration"
+
+    @files = {}
+    now = Time.now
+    @last_flush_cycle = now
+    @last_stale_cleanup_cycle = now
+    flush_interval = @flush_interval.to_i
+    @stale_cleanup_interval = 10
+    conf = Configuration.new
+    @hdfs = FileSystem.get(conf)
+  end # def register
+
+  public
+  def receive(event)
+    return unless output?(event)
+    out = get_output_stream(event.sprintf(@path))
+
+    if @message_format
+      output = event.sprintf(@message_format) + "\n"
+    else
+      output = event.to_json
+    end
+
+    out.write(output)
+
+    flush(out)
+    close_stale_files
+  end # def receive
+
+  def teardown
+    @logger.debug("Teardown: closing files")
+    @files.each do |path, fd|
+      begin
+        fd.close
+        @logger.debug("Closed file #{path}", :fd => fd)
+      rescue Exception => e
+        @logger.error("Excpetion while flushing and closing files.", :exception => e)
+      end
+    end
+    finished
+  end
+
+  private
+  def get_output_stream(path_string)
+    return @files[path_string] if @files.has_key?(path_string)
+    path = Path.new(path_string)
+    if @hdfs.exists(path)
+      if enable_append
+        dfs_data_output_stream = @hdfs.append(path, false)
+      elsif enable_reopen
+        logger.warn "Overwritting HDFS file", :path => path_string
+        dfs_data_output_stream = @hdfs.create(path, true)
+      else
+        raise IOError, "Cowardly refusing to open pre existing file (#{path_string}) because HDFS will truncate the file!"
+      end
+    else
+      dfs_data_output_stream = @hdfs.create(path)
+    end
+    @files[path_string] = DFSOutputStreamWrapper.new(dfs_data_output_stream)
+  end
+
+  def flush(fd)
+    if flush_interval > 0
+      flush_pending_files
+    else
+      fd.flush
+    end
+  end
+
+  # every flush_interval seconds or so (triggered by events, but if there are no events there's no point flushing files anyway)
+  def flush_pending_files
+    return unless Time.now - @last_flush_cycle >= flush_interval
+    @logger.debug("Starting flush cycle")
+    @files.each do |path, fd|
+      @logger.debug("Flushing file", :path => path, :fd => fd)
+      fd.flush
+    end
+    @last_flush_cycle = Time.now
+  end
+
+  # every 10 seconds or so (triggered by events, but if there are no events there's no point closing files anyway)
+  def close_stale_files
+    now = Time.now
+    return unless now - @last_stale_cleanup_cycle >= @stale_cleanup_interval
+    @logger.info("Starting stale files cleanup cycle", :files => @files)
+    inactive_files = @files.select { |path, file| not file.active }
+    @logger.debug("%d stale files found" % inactive_files.count, :inactive_files => inactive_files)
+    inactive_files.each do |path, file|
+      @logger.info("Closing file %s" % path)
+      file.close
+      @files.delete(path)
+    end
+    # mark all files as inactive, a call to write will mark them as active again
+    @files.each { |path, fd| fd.active = false }
+    @last_stale_cleanup_cycle = now
+  end
+
+  class DFSOutputStreamWrapper
+    # reflection locks java objects, so only do this once
+    if org.apache.hadoop.fs.FSDataOutputStream.instance_methods.include? :hflush
+      # hadoop 2.x uses hflush
+      FLUSH_METHOD = :hflush
+    else
+      FLUSH_METHOD = :flush
+    end
+    attr_accessor :active
+    def initialize(output_stream)
+      @output_stream = output_stream
+    end
+    def close
+      @output_stream.close
+    end
+    def flush
+      if FLUSH_METHOD == :hflush
+        @output_stream.hflush
+      else
+        @output_stream.flush
+        @output_stream.sync
+      end
+    end
+    def write(str)
+      bytes = str.to_java_bytes
+      @output_stream.write(bytes, 0, bytes.length)
+      @active = true
+    end
+  end
+end # class LogStash::Outputs::File
+
